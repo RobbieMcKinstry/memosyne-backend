e0d82cb1d3ba23949c216e3e0210eb56aa559688
rushinjgr <jgr10@pitt.edu>
2014-11-08 08:19:31 +0000

    updated methods for Contact

--- Contacts.go
+++ Contacts.go
@@ -1,24 +1,55 @@
 package api
 
-type Contact struct {
-  cid int
-  phone_number string
-  status int
+func (this *Contact) newContact(name string) *Contact {
+    ret := new (Contact)
+    
+    db, err := sql.Open("sqlite3", "sqlite.db")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    rows, err := db.Query("SELECT * FROM Contact WHERE Contact.name=?", name)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    if rows != nil {
+        /* user exists! */
+
+    }
+
+    db.Close()
+
+    return ret
+}
+
+func (this *Contact) delete(id int) bool {
+
+    ret := false
+
+    db, err := sql.Open("sqlite3", "sqlite.db")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    result, err := db.Exec("DELETE FROM Contact WHERE Contact.id=?", id)
+    if err != nil {
+        fmt.Println(err)
+        ret = true
+    }
+
+    db.Close()
+    return ret
+}
+
+func (this *Contact) isAccepted() bool {
+
+    
+    
+    return true
 }
 
 func (this *Contact) save() {
 
 }
 
-func (this *Contact) new() *Contact {
-    ret := new (Contact)
-    return ret
-}
-
-func (this *Contact) delete() bool {
-    return true
-}
-
-func (this *Contact) isAccepted() bool {
-    return true
-}
