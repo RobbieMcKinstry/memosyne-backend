acd0298bd1e7542389e97cea56357b3c28533886
Tyler Raborn <tpr11@pitt.edu>
2014-11-08 09:18:44 -0500

    added initial version of Users.go

--- Users.go
+++ Users.go
@@ -1,21 +1,122 @@
-package api
+/* 
+ *  Methods for the User class
+ *      Written by Tyler Raborn
+ */
 
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/mxk/go-sqlite/sqlite3"
+    "time"
+)
+
+/* UPDATES values in db */
 func (this *User) UserSave() {
-  
+    db := Db_connect()
+
+    result, err := db.Query(
+        "UPDATE User 
+            SET User.phone_num=? AND
+            SET User.first_name=? AND
+            SET User.last_name=? AND
+            SET User.contact_ret=? AND
+            SET User.password=?
+        WHERE User.user_id=?",
+        this.phone_num,
+        this.first_name,
+        this.last_name,
+        this.contact_ret,
+        this.password,
+        this.user_id
+    )
+    result.Close()
+    db.Close()
 }
 
-func (this *User) UserNew() *User {
+/* INSERTS values in db */
+func (this *User) UserNew() bool {
+    ret := true
+    db := Db_connect()
 
+    result, err := db.Query(
+        "INSERT INTO User VALUES (?, ?, ?, ?, ?, ?)",
+        this.phone_num,
+        this.first_name,
+        this.last_name,
+        this.user_id,
+        this.contact_ret,
+        this.password
+    )
+    result.Close()
+    if err != nil {
+        fmt.Println(err)
+        ret = false
+    }
+
+    db.Close()
+    return ret
 }
 
 func (this *User) UserDelete() bool {
+    ret := true
+    db := Db_connect()
+    result, err := db.Query("DELETE FROM User WHERE User.user_id=?", this.user_id)
+    if err != nil {
+        ret = false
+    }
+    result.Close()
+    db.Close()
+    return ret
+}   
 
-}
-
+/*
+ * Returns a slice of pointers to all Contact objects stored in the db
+ */
 func (this *User) GetContacts() []*Contact {
+    db := Db_connect()
+    //contactCount, err := db.Query("SELECT COUNT(*) FROM Contact")
+    contacts, err := db.Query("SELECT * FROM Contact")
+    if err != nil {
+        fmt.Println(err)
+    }
 
+    var contactPointerList []*Contact
+    contactPointerList = make([]*Contact, len(contacts))
+
+    defer contacts.Close()
+    for contacts.Next() {
+        newContactObj := new (Contact)
+
+        var theCid int
+        var thePhoneNum string
+        var theStatus int
+
+        err = contacts.Scan(
+            &theCid, 
+            &thePhoneNum, 
+            &theStatus
+        )
+
+        newContactObj.cid = theCid
+        newContactObj.phone_num = thePhoneNum
+        newContactObj.status = theStatus
+
+        contactPointerList = append(contactPointerList, newContactObj)
+    }
+    contacts.Close()
+    db.Close()
+    return contactPointerList
 }
 
+/* TODO: Finish implementing */
 func (this *User) GetMemos() []*Memos { 
-
+    var memoPointerList []*Memos
+    memoPointerList = make([]*Memo, 1)
+    return memoPointerList
 }
+
+
+
+
