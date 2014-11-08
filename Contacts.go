652c4da12c1656bac0f2794c21c430e7923a533b
Tyler Raborn <tpr11@pitt.edu>
2014-11-08 00:42:30 -0500

    Added intial contacts method definitions

---
+++ Contacts.go
@@ -0,0 +1,22 @@
+
+type Contact struct {
+  cid int
+  phone_number string
+  status int
+}
+
+func (this *Contact) save() {
+
+}
+
+func (this *Contact) new() *contact {
+
+}
+
+func (this *Contact) delete() bool {
+
+}
+
+func (this *Contact) isAccepted() bool {
+    
+}
