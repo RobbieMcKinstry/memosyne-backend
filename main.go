2964ded56ad91c4bfb89380cefc1500d70604e3b
Robbie McKinstry <bravenewprince@gmail.com>
2014-11-12 06:29:34 +0000

    Fake endpoints now send back fake data.

--- main.go
+++ main.go
@@ -8,27 +8,20 @@ import (
 	"net/http"
 )
 
-// TODO
-func HandleContactsRead(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`
-		{ "contacts": [{"name": "Malcomn Reynolds", "number": "+14123444123"}, {"name": "Robbie mcKinstry", "number": "+14124453171"}]
-		}
-	`))
-}
-func HandleMemoCreate(w http.ResponseWriter, r *http.Request) {
-}
-
 func main() {
 	log.Println("Hello Memosyne")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/sessions/{id}", HandleSessionRead).Methods("GET")
 	r.HandleFunc("/sessions", HandleSessionCreate).Methods("POST")
+
 	r.HandleFunc("/users/{id}/contacts", HandleContactsRead).Methods("GET")
-	r.HandleFunc("/users/{id}", HandleUserRead).Methods("GET")
-	r.HandleFunc("/memos", HandleMemoCreate).Methods("POST")
 	r.HandleFunc("/users/{id}/memos", HandleMemoRead).Methods("GET")
+	r.HandleFunc("/users/{id}", HandleUserRead).Methods("GET")
 	r.HandleFunc("/users", HandleUserCreate).Methods("POST")
+
+	r.HandleFunc("/memos", HandleMemoCreate).Methods("POST")
+
 	r.HandleFunc("/contacts", HandleContactsCreate).Methods("POST")
 	r.HandleFunc("/", HandleHelloWorld)
 	http.Handle("/", r)
@@ -37,21 +30,6 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func HandleMemoRead(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	sessionId := vars["id"]
-	_ = sessionId
-	// get all memos out of the database for a user.
-	// seralize those memos into json.
-	// then write that json
-	w.Write([]byte(`{
-		"memos": [{"body": "Please, Jenny! I love you!"}, {"body": "I aim to misbehave!"}]
-	}`))
-}
-
-func HandleContactsCreate(w http.ResponseWriter, r *http.Request) {
-	// TODO implement
-}
 
 func HandleSessionRead(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -60,18 +38,10 @@ func HandleSessionRead(w http.ResponseWriter, r *http.Request) {
 	output := fmt.Sprintf("Hello, session %v", sessionId)
 	_ = output
 
-	// sample user object
-	s := `{
-		"user_id":      "19",
-		"first_name":   "Robbie",
-		"last_name":    "McKinstry",
-		"phone_number": "412-445-3171",
-	}`
-	// what else goes in here? not contacts.
-	// need /users/:id/memos
-	// need /users/:id/contacts
+	s := `{ "session_id": 120, }`
 	w.Write([]byte(s))
 }
+
 
 func HandleSessionCreate(w http.ResponseWriter, r *http.Request) {
 	/* TODO get the json out of the request.
@@ -86,7 +56,60 @@ func HandleSessionCreate(w http.ResponseWriter, r *http.Request) {
 	// return that id
 
 	s := `{
-		"session_id": 29  
+		"session_id": 29,
+		"user_id":    120,
+	}`
+	w.Write([]byte(s))
+}
+
+
+func HandleContactsRead(w http.ResponseWriter, r *http.Request) {
+	s := `{
+		“contacts”: [ 
+			{ 
+				“contact_id”: 1,
+				“phone_number”: “412-445-3171”,
+				“first_name”:	“Robbie”,
+				“last_name”:	“McKinstry”, 
+			}, {
+				“contact_id”: 2,
+				“phone_number”: “412-661-2963”,
+				“first_name”:	“Malcolm”,
+				“last_name”:	“Reynolds”, 
+			}
+		],
+	}`
+	w.Write([]byte(s))
+}
+
+
+func HandleMemoRead(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	sessionId := vars["id"]
+	_ = sessionId
+	// get all memos out of the database for a user.
+	// seralize those memos into json.
+	// then write that json
+	s := `{
+
+		“sender_id”:	0, 		
+		“contact_id”:	0, 		
+		“body”:		“”, 		
+		“time”:		“”,
+
+		"memos": [
+			{
+				"sender_id": 7,
+				"contact_id": 99,
+				"body": "Please, Jenny! I love you!",
+				"time": "2006-01-02T15:04:05Z07:00",
+			}, {
+				"sender_id": 8,
+				"contact_id": 100,
+				"body": "I aim to misbehave!"
+				"time": "2006-01-02T15:04:05Z07:00",
+			},
+		]
 	}`
 	w.Write([]byte(s))
 }
@@ -100,13 +123,12 @@ func HandleUserRead(w http.ResponseWriter, r *http.Request) {
 
 	s := `{
 		"user": {
-			"user_id": %v,
-			"first_name": %v,
-			"last_name": %v,
-			"email_address": %v,
-		}
+			"user_id": 12,
+			"first_name": "Nigel",
+			"last_name": "Thornberry",
+			"email_address": "wounderous_animals@thornberry.tv",
+		},
 	}`
-	// s = fmt.Sprintf(s, _, _, _, _)
 	w.Write([]byte(s))
 }
 
@@ -114,9 +136,40 @@ func HandleUserRead(w http.ResponseWriter, r *http.Request) {
 // TODO determine what I should be giving back. Then give it back.
 func HandleUserCreate(w http.ResponseWriter, r *http.Request) {
 	s := `{
+		"session_id": 13,
+		"user_id":    201,
 	}`
 	w.Write([]byte(s))
 }
+
+
+func HandleMemoCreate(w http.ResponseWriter, r *http.Request) {
+	s :=` {
+		"memos": {
+			"sender_id": 7,
+			"contact_id": 99,
+			"body": "Please, Jenny! I love you!",
+			"time": "2006-01-02T15:04:05Z07:00",
+		},
+	}`
+	w.Write([]byte(s))
+}
+
+
+func HandleContactsCreate(w http.ResponseWriter, r *http.Request) {
+	// TODO implement
+	s := `{
+		“contact”: { 
+			“contact_id”: 3,
+			“phone_number”: “724-903-3112”,
+			“first_name”:	“Robert”,
+			“last_name”:	“Floyd”,
+		},
+	}`
+
+	w.Write([]byte(s))
+}
+
 
 func HandleHelloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, Memosyne!"))
